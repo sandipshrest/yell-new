721e8236a0c9465ad81aadbae5119e22eb09c802
Sandip Shrestha <c.restsandip123@gmail.com>
2025-09-07 22:16:51 +0545

    feat: add axios instance and layout component with react-query integration

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Layout;
